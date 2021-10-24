6f96b69b0ae75cc3b31ef210f7a72ab16c59e924
jdwcg <bigcloudjo@gmail.com>
2021-10-24 22:08:27 +0900

    4

--- assets/script.js
+++ assets/script.js
@@ -20,3 +20,19 @@ offwork.addEventListener('click', function() {
     offworkOff.style.display = 'block';
     offworkOff.style.color = '#8C64FF';
 });
+
+var aside = document.querySelector('.aside');
+var asideBg = document.querySelector('.asideBg');
+document.querySelector('header span:nth-child(2)').addEventListener('click', function(){
+   aside.style.display='block';
+   asideBg.style.display='block';
+});
+
+document.querySelector('.aside > i').addEventListener('click', function(){
+   aside.style.display='none';
+   asideBg.style.display='none';
+})
+document.querySelector('.asideBg').addEventListener('click', function(){
+   aside.style.display='none';
+   asideBg.style.display='none';
+})
