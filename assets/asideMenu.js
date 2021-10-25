32b2a4ad20e03fe4432f1b806a674533d744342b
jdwcg <bigcloudjo@gmail.com>
2021-10-25 23:45:32 +0900

    20

--- assets/asideMenu.js
+++ assets/asideMenu.js
@@ -1,15 +1,16 @@
 var aside = document.querySelector('.aside');
 var asideBg = document.querySelector('.asideBg');
 document.querySelector('header span:nth-child(2)').addEventListener('click', function(){
-   aside.style.display='block';
+   // aside.style.display='block';
+   aside.classList.add('moveLeft-class');
    asideBg.style.display='block';
 });
 
 document.querySelector('.aside > i').addEventListener('click', function(){
-   aside.style.display='none';
+   aside.classList.remove('moveLeft-class');
    asideBg.style.display='none';
 })
 document.querySelector('.asideBg').addEventListener('click', function(){
-   aside.style.display='none';
+   aside.classList.remove('moveLeft-class');
    asideBg.style.display='none';
 })
