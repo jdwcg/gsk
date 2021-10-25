988b646cf9b86b46e8c9321308a08d6473c6dce8
jdwcg <73451502+jdwcg@users.noreply.github.com>
2021-10-25 19:36:31 +0900

    15

---
+++ assets/company.js
@@ -0,0 +1,55 @@
+$(function(){
+$('.content > section:nth-child(2) > article:nth-child(1)').show('easing');
+$('.content > section:nth-child(2) > article:nth-child(2)').hide('easing');
+$('.content > section:nth-child(2) > article:nth-child(3)').hide('easing');
+
+$('.tabBtn > button:nth-child(1)').click(function(){
+  $('.content > section:nth-child(2) > article:nth-child(1)').show('easing');
+  $('.content > section:nth-child(2) > article:nth-child(2)').hide('easing');
+  $('.content > section:nth-child(2) > article:nth-child(3)').hide('easing');
+});
+$('.tabBtn > button:nth-child(2)').click(function(){
+  $('.content > section:nth-child(2) > article:nth-child(1)').hide('easing');
+  $('.content > section:nth-child(2) > article:nth-child(2)').show('easing');
+  $('.content > section:nth-child(2) > article:nth-child(3)').hide('easing');
+});
+$('.tabBtn > button:nth-child(3)').click(function(){
+  $('.content > section:nth-child(2) > article:nth-child(1)').hide('easing');
+  $('.content > section:nth-child(2) > article:nth-child(2)').hide('easing');
+  $('.content > section:nth-child(2) > article:nth-child(3)').show('easing');
+});
+
+})
+//
+// var tabBtn1 = $('.tabBtn > button:nth-child(1)');
+// var tabBtn2 = $('.tabBtn > button:nth-child(2)');
+// var tabBtn3 = $('.tabBtn > button:nth-child(3)');
+// var tabCtn1 = document.querySelector('.content > section:nth-child(2) > article:nth-child(1)');
+// var tabCtn2 = document.querySelector('.content > section:nth-child(2) > article:nth-child(2)');
+// var tabCtn3 = document.querySelector('.content > section:nth-child(2) > article:nth-child(3)');
+//
+// tabCtn1.style.display = "block";
+// tabCtn2.style.display = "none";
+// tabCtn3.style.display = "none";
+//
+// tabBtn1.click(function(){
+//   if(tabCtn1.style.display === "none"){
+//     tabCtn1.style.display = "block";
+//     tabCtn2.style.display = "none";
+//     tabCtn3.style.display = "none";
+//   };
+// });
+// tabBtn2.click(function(){
+//   if(tabCtn2.style.display === "none"){
+//     tabCtn1.style.display = "none";
+//     tabCtn2.style.display = "block";
+//     tabCtn3.style.display = "none";
+//   };
+// });
+// tabBtn3.click(function(){
+//   if(tabCtn3.style.display === "none"){
+//     tabCtn1.style.display = "none";
+//     tabCtn2.style.display = "none";
+//     tabCtn3.style.display = "block";
+//   };
+// });
