3af6cb301f626e7f90c113c08856707f7aa32050
jdwcg <73451502+jdwcg@users.noreply.github.com>
2021-11-09 14:54:27 +0900

    24

--- assets/acc.js
+++ assets/acc.js
@@ -12,16 +12,44 @@ $(function(){
   $('.content').hide();
   $('.bi-chevron-up').hide();
   $('.content').eq(0).show();
-   $('.toggleBar').click(function() {
+   $('.toggleBar1').click(function(e) {
+     e.preventDefault();
      $(this).next().slideToggle();
 
-     // if( $('.accordion > .toggleBar > span .bi-chevron-up').css('display') === 'none' ) {
-     //   $(this).children('span').children('i:first-child').css({display: 'none'});
-     //   $(this).children('span').children('i:last-child').css({display: 'block'});
-     // } else {
-     //   $(this).children('span').children('i:first-child').css({display: 'block'});
-     //   $(this).children('span').children('i:last-child').css({display: 'none'});
-     // }
-
+     if( $('.accordion > .toggleBar1 > span .bi-chevron-up').css('display') === 'none' ) {
+       $(this).children('span').children('i:first-child').css({display: 'none'});
+       $(this).children('span').children('i:last-child').css({display: 'block'});
+     } else {
+       $(this).children('span').children('i:first-child').css({display: 'block'});
+       $(this).children('span').children('i:last-child').css({display: 'none'});
+     }
    })
+   $('.toggleBar2').click(function(e) {
+     e.preventDefault();
+     $(this).next().slideToggle();
+
+     if( $('.accordion > .toggleBar2 > span .bi-chevron-up').css('display') === 'none' ) {
+       $(this).children('span').children('i:first-child').css({display: 'none'});
+       $(this).children('span').children('i:last-child').css({display: 'block'});
+     } else {
+       $(this).children('span').children('i:first-child').css({display: 'block'});
+       $(this).children('span').children('i:last-child').css({display: 'none'});
+     }
+   })
+   $('.toggleBar3').click(function(e) {
+     e.preventDefault();
+     $(this).next().slideToggle();
+
+     if( $('.accordion > .toggleBar3 > span .bi-chevron-up').css('display') === 'none' ) {
+       $(this).children('span').children('i:first-child').css({display: 'none'});
+       $(this).children('span').children('i:last-child').css({display: 'block'});
+     } else {
+       $(this).children('span').children('i:first-child').css({display: 'block'});
+       $(this).children('span').children('i:last-child').css({display: 'none'});
+     }
+   })
+
+
+
+
 })
